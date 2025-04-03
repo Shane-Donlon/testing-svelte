8168b118dd6e67672cdc1cb65d2ff0ead02d1125
Shane-Donlon <130906067+Shane-Donlon@users.noreply.github.com>
2025-04-03 10:01:46 +0100

    svelte counter add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import svelte from "@astrojs/svelte";
+
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [svelte()],
+  adapter: vercel(),
+});
